14a3c39b1739eab976427816e581aa77adb1075e
karuna reddy <karunaram987@gmail.com>
2018-01-27 20:45:21 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Karuna Reddy';
+ 
+  user: Observable<firebase.User>;
+  msgVal: string = '';
+  items:any=[];
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = this.af.list('/messages');
+
+    this.user = this.afAuth.authState;
+
+  }  
+
+
+  login() {
+    this.afAuth.auth.signInAnonymously();
+}
+
+logout() {
+    this.afAuth.auth.signOut();
+}
+
+Send(desc: string) {
+    this.items.push({ message: desc});
+    this.msgVal = '';
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,77 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {Routes,Router, RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import {AngularFireModule} from 'angularfire2';
+
+import { AngularFireDatabaseModule, AngularFireDatabaseProvider } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+// for AngularFireAuth
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { FileSaverModule } from 'ngx-filesaver'; 
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCWt1Ei_lLIPXyT95GXSiHC76_RvDi9qIw",
+  authDomain: "my-profile-69f77.firebaseapp.com",
+  databaseURL: "https://my-profile-69f77.firebaseio.com",
+  projectId: "my-profile-69f77",
+  storageBucket: "my-profile-69f77.appspot.com",
+  messagingSenderId: "613979181676"
+};
 
 
+
+const routes: Routes =[
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+ 
+  {path: 'welcome',component: WelcomeComponent},
+  {path:'portfolio', component: PortfolioComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'contact', component: ContactmeComponent}
+ ]; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ContactmeComponent,
+    JumbotronComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MDBBootstrapModule.forRoot(),
+    NgbModule.forRoot(),    
+    RouterModule.forRoot(routes,{enableTracing: true}),
+    AngularFireModule.initializeApp(firebaseConfig),
+  AngularFireDatabaseModule,
+  AngularFireAuthModule,
+  FileSaverModule   
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:      [ NO_ERRORS_SCHEMA ]
 })
-export class AppModule { }
+export class AppModule {
+
+  
+ }
+
+ AngularFireDatabaseModule
